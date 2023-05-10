211e9f6f4639a9d04061fb84430f7e13b23d7181
Alfie <alfye123@hotm.com>
2023-05-11 00:20:58 +0100

    added some navbar functionality using materialui altered the icons and their base values

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,32 @@
+import { useState } from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Storage from '@mui/icons-material/Storage';
+import BarChartIcon from '@mui/icons-material/BarChart';
+
 export default function Navbar() {
-  return <nav className="navbar">Name</nav>;
+  const [value, setValue] = useState();
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation
+      sx={{ position: 'relative' }}
+      value={value}
+      onChange={handleChange}
+    >
+      <BottomNavigationAction
+        label="Manage"
+        value="manage"
+        icon={<Storage />}
+      />
+      <BottomNavigationAction
+        label="Charts"
+        value="charts"
+        icon={<BarChartIcon />}
+      />
+    </BottomNavigation>
+  );
 }
